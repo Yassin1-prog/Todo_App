466d92a0f18003c7d2e5ccd43972f9ca5fffdb26
Yassin <132544868+Yassin1-prog@users.noreply.github.com>
2024-03-02 20:49:37 +0200

    bugs fixed and responsive made

--- src/index.js
+++ src/index.js
@@ -1,203 +1,211 @@
 import createTodo from "./createTodo";
 import createProject from "./createProject";
 import { projectLoad } from "./loadProject";
+import { format, parseISO } from "date-fns";
+
+//console.log(localStorage);
+//localStorage.clear();
 
 function loadAllProjectsFromLocalStorage() {
-    const projects = [];
-    for (let i = 0; i < localStorage.length; i++) {
-        const projectName = localStorage.key(i);
-        const projectData = localStorage.getItem(projectName);
-        const projectTasks = JSON.parse(projectData).map(
-            ({ title, description, date, priority }) =>
-                new createTodo(title, description, date, priority)
-        );
+  const projects = [];
+  const inbox = new createProject("Inbox");
+  projects.push(inbox);
+  for (let i = 0; i < localStorage.length; i++) {
+    const projectName = localStorage.key(i);
 
-        const project = new createProject(projectName);
-        project.project = projectTasks;
-        projects.push(project);
+    const projectData = localStorage.getItem(projectName);
+    const projectTasks = JSON.parse(projectData).map(
+      ({ title, description, date, priority }) =>
+        new createTodo(title, description, date, priority)
+    );
+    if (projectName == "Inbox") {
+      projects[0].project = projectTasks;
+    } else {
+      const project = new createProject(projectName);
+      project.project = projectTasks;
+
+      projects.push(project);
     }
-    return projects;
+  }
+  return projects;
 }
 
 const projects = loadAllProjectsFromLocalStorage();
 
-//const projects = [];
-const inbox = new createProject('Inbox');
-projects.push(inbox);
-
 const inbx = document.querySelector(".inbox");
 inbx.addEventListener("click", () => {
-    projectLoad(projects[0]);
-    updateTasks(projects[0]);
-    inbox.saveToLocalStorage();
+  projectLoad(projects[0]);
+  updateTasks(projects[0]);
+  projects[0].saveToLocalStorage();
 });
-
-
 
 const dialog = document.querySelector("[data-modal]");
 const openButton = document.querySelector(".addTask");
 const closeButton = document.querySelector(".close");
 
-const cleanInput = document.querySelectorAll('[title], [description], [date], [priority]');
-const taskHolder = document.querySelector('.tasks');
+const cleanInput = document.querySelectorAll(
+  "[title], [description], [date], [priority]"
+);
+const taskHolder = document.querySelector(".tasks");
 
-
-const title = document.querySelector('[title]');
-const description = document.querySelector('[description]');
-const date = document.querySelector('[date]');
-const priority = document.querySelector('[priority]');
+const title = document.querySelector("[title]");
+const description = document.querySelector("[description]");
+const date = document.querySelector("[date]");
+const priority = document.querySelector("[priority]");
 
 openButton.addEventListener("click", () => {
-    dialog.showModal();
+  dialog.showModal();
 });
 
 closeButton.addEventListener("click", (e) => {
-    e.preventDefault();
-    
-    let target;
+  e.preventDefault();
 
-    const whichProject = document.querySelector('.title');
+  let target;
 
-    for(let i = 0; i < projects.length; i++) {
-        if(projects[i].name === whichProject.textContent) {
-             target = projects[i];
-        }
+  const whichProject = document.querySelector(".title");
+
+  for (let i = 0; i < projects.length; i++) {
+    if (projects[i].name === whichProject.textContent) {
+      target = projects[i];
     }
+  }
 
-    let flag = false;
-    for(let i = 0; i < target.project.length; i++) {
-        if(target.project[i].getTitle() == title.value) {
-            flag = true;
-            target.project[i].description = description.value;
-            target.project[i].date = date.value;
-            target.project[i].priority = priority.value;
-        }
+  let flag = false;
+  for (let i = 0; i < target.project.length; i++) {
+    if (target.project[i].getTitle() == title.value) {
+      flag = true;
+      target.project[i].description = description.value;
+      target.project[i].date = date.value;
+      target.project[i].priority = priority.value;
+      target.saveToLocalStorage();
     }
+  }
 
-    if(!flag) {
-        target.addProject(title.value, description.value, date.value, priority.value);
-        target.saveToLocalStorage();
-    }
+  if (!flag) {
+    target.addProject(
+      title.value,
+      description.value,
+      date.value,
+      priority.value
+    );
+    target.saveToLocalStorage();
+  }
 
-    updateTasks(target);
+  updateTasks(target);
 
-    cleanInput.forEach( input => {
-         input.value = '';
-    });
+  cleanInput.forEach((input) => {
+    input.value = "";
+  });
 
-    dialog.close();
+  dialog.close();
 });
 
-
-const projectButton = document.querySelector('.addProject');
-const inputContainer = document.querySelector('.inputContainer');
-const projectName = document.createElement('input');
-const list = document.querySelector('.list');
-projectName.type = 'text';
-projectName.name = 'project';
-projectName.placeholder = 'name';
-const confirmButton = document.createElement('button');
+const projectButton = document.querySelector(".addProject");
+const inputContainer = document.querySelector(".inputContainer");
+const projectName = document.createElement("input");
+const list = document.querySelector(".list");
+projectName.type = "text";
+projectName.name = "project";
+projectName.placeholder = "name";
+projectName.classList.add("inputP");
+const confirmButton = document.createElement("button");
 confirmButton.textContent = "Confirm";
-confirmButton.classList.add('Confirm');
+confirmButton.classList.add("Confirm");
 
 projectButton.addEventListener("click", () => {
-    inputContainer.appendChild(projectName);
-    inputContainer.appendChild(confirmButton);
+  inputContainer.appendChild(projectName);
+  inputContainer.appendChild(confirmButton);
 });
 
-
 confirmButton.addEventListener("click", () => {
-    const createdProject = new createProject(projectName.value);
-    projects.push(createdProject);
-    createdProject.saveToLocalStorage();
-    inputContainer.innerHTML = '';
-    projectName.value = '';
-    updateProjects();
+  const createdProject = new createProject(projectName.value);
+  projects.push(createdProject);
+  createdProject.saveToLocalStorage();
+  inputContainer.innerHTML = "";
+  projectName.value = "";
+  updateProjects();
 });
 
 function updateProjects() {
-    list.innerHTML = '';
-    projects.forEach((project, index) => {
+  list.innerHTML = "";
+  projects.forEach((project, index) => {
+    const newProject = document.createElement("button");
+    newProject.textContent = project.name;
+    newProject.classList.add("projectName");
+    newProject.setAttribute("data-index", index);
 
-        const newProject = document.createElement('button');
-        newProject.textContent = project.name;
-        newProject.classList.add("projectName");
-        newProject.setAttribute("data-index", index);
+    if (index !== 0) list.appendChild(newProject);
 
-        if(index!==0) list.appendChild(newProject);
-
-        newProject.addEventListener("click",  () => {
-            let rowIndex = parseInt(newProject.getAttribute("data-index"));
-            projectLoad(projects[rowIndex]);
-            updateTasks(projects[rowIndex]);
-        });
-
+    newProject.addEventListener("click", () => {
+      let rowIndex = parseInt(newProject.getAttribute("data-index"));
+      projectLoad(projects[rowIndex]);
+      updateTasks(projects[rowIndex]);
     });
+  });
 }
 
 function updateTasks(project) {
-    taskHolder.textContent = '';
-    
-    project.project.forEach((task, index) => {
-        const btn = document.createElement('button');
-        btn.textContent = task.getTitle();
-        btn.setAttribute("data-index", index);
-        btn.classList.add('createdButton');
-        
-        if(task.getPriority() === "low") {
-            btn.style.borderLeft = '4px solid green';
-        }
-        else if(task.getPriority() === "high") {
-            btn.style.borderLeft = '4px solid red'
-        }
-        else {
-            btn.style.borderLeft = '4px solid orange'
-        }
+  taskHolder.textContent = "";
 
-        const dueDate = document.createElement('div');
-        dueDate.textContent = task.getDate();
-        dueDate.classList.add('datee');
+  project.project.forEach((task, index) => {
+    const btn = document.createElement("button");
+    btn.textContent = task.getTitle();
+    btn.setAttribute("data-index", index);
+    btn.classList.add("createdButton");
 
-        const removeButton = document.createElement('button');
-        removeButton.textContent = 'remove';
-        removeButton.classList.add('move');
-        removeButton.setAttribute("data-index", index);
+    if (task.getPriority() === "low") {
+      btn.style.borderLeft = "4px solid green";
+    } else if (task.getPriority() === "high") {
+      btn.style.borderLeft = "4px solid red";
+    } else {
+      btn.style.borderLeft = "4px solid orange";
+    }
 
-        taskHolder.appendChild(btn);
-        taskHolder.appendChild(dueDate);
-        taskHolder.appendChild(removeButton);
+    const dueDate = document.createElement("div");
+    dueDate.textContent = format(parseISO(task.getDate()), "dd-MM-yyyy");
+    dueDate.classList.add("datee");
 
-        btn.addEventListener("click", () => {
-            let rowIndex = parseInt(btn.getAttribute("data-index"));
-            loadTaskInfo(project.project[rowIndex]);
-        });
+    const removeButton = document.createElement("button");
+    removeButton.textContent = "remove";
+    removeButton.classList.add("move");
+    removeButton.setAttribute("data-index", index);
 
-        removeButton.addEventListener("click", () =>  {
-            let colIndex = parseInt(removeButton.getAttribute("data-index"));
-            project.project.splice(colIndex, 1);
-            project.saveToLocalStorage();
-            updateTasks(project);
-        });
-    });  
+    taskHolder.appendChild(btn);
+    taskHolder.appendChild(dueDate);
+    taskHolder.appendChild(removeButton);
+
+    btn.addEventListener("click", () => {
+      let rowIndex = parseInt(btn.getAttribute("data-index"));
+      loadTaskInfo(project.project[rowIndex]);
+    });
+
+    removeButton.addEventListener("click", () => {
+      let colIndex = parseInt(removeButton.getAttribute("data-index"));
+      project.project.splice(colIndex, 1);
+      project.saveToLocalStorage();
+      updateTasks(project);
+    });
+  });
 }
 
 function loadTaskInfo(task) {
-    dialog.showModal();
-    title.value = task.getTitle();
-    description.value = task.getDescription();
-    date.value = task.getDate();
-    priority.value = task.getPriority();
-
+  dialog.showModal();
+  title.value = task.getTitle();
+  description.value = task.getDescription();
+  date.value = task.getDate();
+  priority.value = task.getPriority();
 }
 
-
-window.addEventListener('click', (e) => {
-    if (e.target === dialog) {
-      dialog.close();
-    }
+window.addEventListener("click", (e) => {
+  if (e.target === dialog) {
+    cleanInput.forEach((input) => {
+      input.value = "";
+    });
+    dialog.close();
+  }
 });
 
 updateProjects();
-for(let i = 0; i < projects.length; i++) {
-    updateTasks(projects[i]);
+for (let i = projects.length - 1; i > -1; i--) {
+  updateTasks(projects[i]);
 }
